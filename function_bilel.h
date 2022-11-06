0131d8053b71c95c2e9400ead78ff7d734456853
Bilel DHIFI <bileldhifi37@gmail.com>
2022-11-06 22:34:37 +0100

    Update function_bilel.h

--- function_bilel.h
+++ function_bilel.h
@@ -1,1 +1,26 @@
+#ifndef POINT_H_INCLUDED
+#define POINT_H_INCLUDED
+#include <stdio.h>
+typedef struct
+{
+int jour;
+int mois;
+int annee;
+}datenais;
+typedef struct
+{
+    char nom[50];
+    char prenom[50];
+    int id;
+    char mot_de_passe[50];
+    char role[50];
+    bool sex;
+    datenais date;
+    char municipalite[50];
+    int numero;
+} form;
 
+int ajouter();
+
+
+#endif // POINT_H_INCLUDED
